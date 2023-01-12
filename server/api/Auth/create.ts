78450f8ac2220fffe73219631db69df483ea57be
Muhammet Emin Güleşci <114227722+EminJ@users.noreply.github.com>
2023-01-12 17:53:38 +0300

    Auth 🥶

--- server/api/Auth/create.ts
+++ server/api/Auth/create.ts
@@ -1,22 +1,33 @@
 import express from "express";
 const router = express.Router()
+import bcrypt from 'bcrypt'
+import jwt from 'jsonwebtoken';
+import Users from '../model/users'
 
-router.post('/',(req,res)=>{
+router.post('/',async (req,res)=>{
 	const {name,email,password}=req.body
-	if(!name || !email || !password){
-		return res.status(400).send(message('Eksik Veri Girişi!',400))
-	}
-	if (!isNameValid(name)) {
-		return res.status(400).send(message('İsim En Az 3 Harfli Olmalıdır!',400))
-	}
-	if (!isEmailValid(email)) {
-		return res.status(400).send(message('Geçersiz E-Posta Adresi Girişi!',400))
-	}
-	if (!isPasswordValid(password)) {
-		return res.status(400).send(message('Şifre Girişi En Az 8 Karakterli Ve En Az 1 Büyük Harf İçermeli!',400))
-	}
+	if(!name || !email || !password){return res.status(400).send(message('Eksik Veri Girişi!',400))}
+	if (!isNameValid(name)) {return res.status(400).send(message('İsim En Az 3 Harfli Olmalıdır!',400))}
+	if (!isEmailValid(email)) {return res.status(400).send(message('Geçersiz E-Posta Adresi Girişi!',400))}
+	if (!isPasswordValid(password)) {return res.status(400).send(message('Şifre Girişi En Az 8 Karakterli Ve En Az 1 Büyük Harf İçermeli!',400))}
+
+	const existingUser = await Users.findOne({ email });
+    if (existingUser) {
+      return res.status(400).send(message('Bu E-Posta Adresi Zaten Kullanılıyor!',400));
+    }
+	const hashedPassword = await bcrypt.hash(password, 10);
+
+	new Users({ name, email, password: hashedPassword })
+	.save()
+	.then((user)=>{
+		const token = jwt.sign(user, process.env.SECURTY_KEY);
+		console.log(token);
+		return res.status(200).send(message('Tebrikler, Hesap Oluşturuldu!',200))
+	})
+	.catch((err)=>{
+		return res.status(500).send(message('Kayıt Başarısız, Sistemde Bir Hata Olmuş Olmalı!',500))
+	})
 	
-	return res.status(200).send(message('Tebrikler, Hesap Oluşturuldu!',200))
 })
 
 function message(message,status) {return {send:'Istanbul Tablo',message:message,status:status}}
